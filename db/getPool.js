b88d7a1abb434ca72d38d7dfaf6c38c9d3ca9c98
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-12-09 16:20:29 -0300

    initDB

--- db/getPool.js
+++ db/getPool.js
@@ -1,38 +1,35 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
 
-const {
-    MYSQL_HOST,
-    MYSQL_USER,
-    MYSQL_PASSWORD,
-    MYSQL_DB
-} = process.env;
+dotenv.config();
 
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
 
 let pool;
 
-const getPool = async() =>{
-    try{
-        if(!pool){
-            const poolTem = mysql.createPool({
-                host: MYSQL_HOST,
-                user: MYSQL_USER,
-                password: MYSQL_PASSWORD
-            })
-            await poolTem.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+const getPool = async () => {
+  try {
+    if (!pool) {
+      const poolTem = mysql.createPool({
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASS,
+      });
+      await poolTem.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
 
-            pool = mysql.createPool({
-                host: MYSQL_HOST,
-                user: MYSQL_USER,
-                password: MYSQL_PASSWORD,
-                database: MYSQL_DB,
-                connectionLimit: 10,
-                timezone: 'Z'
-            });
-        }
-        return pool;
-    }catch (err){
-        console.error(err);
+      pool = mysql.createPool({
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASS,
+        database: MYSQL_DB,
+        connectionLimit: 10,
+        timezone: "Z",
+      });
     }
-}
+    return pool;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = getPool;
