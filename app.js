9c0f952120397aa2931dece237fa4013845b5edf
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-12-10 17:28:35 -0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const router = require('./routes/index.routes');
+const errorController = require('./controllers/error/index.controller');
 
 dotenv.config();
 const {HTTP_PORT} = process.env;
 const app = express();
 
 app.use(router);
+
+app.use(errorController);
 
 app.listen(HTTP_PORT, ()=>{
     console.log('http://localhost:' + HTTP_PORT);
